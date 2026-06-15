6eaaf13de54412570bdb3006b67599764b2a66bf
Small_Butterscotch18 <kingston.kdk@gmail.com>
2026-06-15 11:49:24 +0800

    updated next for newclear.website

--- components/ui/notes/experiments/newclear.ts
+++ components/ui/notes/experiments/newclear.ts
@@ -8,7 +8,7 @@ const newclear: Note = {
   body: '',
   sections: [
     { heading: 'info', body: 'subpage of cli-mate.help. acts as an interactive info hub for nuclear electricity education. demystifying nuclear goos and danger myths, providing facts and figures to help Singaporeans understand the benefits of nuclear energy.' },
-    { heading: 'next', body: '', checklist: true },
+    { heading: 'next', body: '\n check if languages are translated correctly \n maybe add images to background?', checklist: true },
     { heading: 'current', body: 'https://newclear.website'},
   ],
   pinned: false,
