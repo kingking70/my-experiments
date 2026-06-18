f96c5258a39625d208477c68054f40c1898761e0
Small_Butterscotch18 <kingston.kdk@gmail.com>
2026-06-18 18:41:26 +0800

    updated thewanderclan details

--- components/ui/notes/experiments/thewanderclan.ts
+++ components/ui/notes/experiments/thewanderclan.ts
@@ -7,8 +7,8 @@ const thewanderclan: Note = {
   subtitle: 'mobile app concept 2023',
   body: '',
   sections: [
-    { heading: 'info', body: 'the wander clan is a private bespoke travel agency.' },
-    { heading: 'considerations', body: 'this is a concept mobile app based on a real company. \n\na bespoke travel app for private clients, included inspired features from luxury brands mobile apps. \n\ndata collection and processing is minimal. the service is made after the first encounter with clients, where the company would register the client\'s details for authentication and sign in. the app serves as a content inspiration piece where clients can read articles about their next trip and a secondary platform for clients to register their intent to travel with the company. the main pipeline is still through physical bespoke contact.' },
+    { heading: 'info', body: 'the wander clan was a private bespoke travel agency that turned into a private bespoke consulting firm.' },
+    { heading: 'considerations', body: 'this is a concept mobile app based on a real company back in 2023/4. \n\na bespoke travel app for private clients, included inspired features from luxury brands mobile apps. \n\ndata collection and processing is minimal. registration is made after the first encounter with clients, in which the client\'s details are registered for authentication and sign in. the app serves as a content inspiration piece where clients can read articles about their next trip and a secondary platform for clients to register their intent to travel with the company. the main pipeline is still through physical bespoke contact.' },
     { heading: 'current', body: '', video: '/thewanderclan-current.mp4' },
   ],
   pinned: false,
