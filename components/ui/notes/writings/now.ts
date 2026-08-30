2825656a7b3716e79bdcd9cb74afe82b9f5335da
Small_Butterscotch18 <kingston.kdk@gmail.com>
2026-08-30 12:17:17 +0800

    updated in accordance with reality

--- components/ui/notes/writings/now.ts
+++ components/ui/notes/writings/now.ts
@@ -4,17 +4,17 @@ const now: Note = {
   id: 'now',
   folder: 'writings',
   title: 'now',
-  subtitle: '3 aug 2026',
+  subtitle: '30 aug 2026',
   body: '',
   pinned: false,
   sections: [
     {
       heading: 'what i\'m up to',
-      body: 'preparing my physical and mental state for work beginning 17 aug 2026.'
+      body: 'experimenting.'
     },
     {
       heading: 'next chapter',
-      body: 'i still want to build worlds through literature, art, philosophy, and tech. i will begin with writing and experimenting with small tech-based projects.'
+      body: ''
     }
   ],
 };
